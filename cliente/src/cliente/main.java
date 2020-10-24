e51e3eea330fe339af282bf149525238d5daba9d
Alberto Fernandez <albertofer97@gmail.com>
2020-10-24 19:10:57 -0300

    agregacion de capturas de pantallas

--- cliente/src/cliente/main.java
+++ cliente/src/cliente/main.java
@@ -17,24 +17,7 @@ public class main {
 
 	public static void main(String[] args) {
 
-		// Movil m= new Movil(4,"lancha");
-		// Movil m2= new Movil(4,150,150);
-		// Client cliente = ClientBuilder.newClient();
-		// WebTarget target1 =
-		// cliente.target("http://localhost:8080/moviles/rest/").path("moviles");
-		// WebTarget target2 =
-		// cliente.target("http://localhost:8080/moviles/rest/").path("/moviles/registro");
-		// String[] s =new String[1];
-		// s[0]=MediaType.APPLICATION_JSON;
-		// Invocation.Builder invocationBuilder1 = target1.request(s);
-		// Invocation.Builder invocationBuilder2 = target2.request(s);
-		// Response response1 = invocationBuilder1.post(Entity.entity(m,
-		// MediaType.APPLICATION_JSON));
-		// Response response2 = invocationBuilder2.post(Entity.entity(m2,
-		// MediaType.APPLICATION_JSON));
-		// System.out.println(response1.getStatus());
-		// System.out.println(response2.getStatus());
-		// obtenerPorUbicacion(100,100,213400000);
+	
 		menu();
 
 	}
@@ -50,9 +33,8 @@ public class main {
 			System.out.println("3- Registrar ubicacion de un movil");
 			System.out.println("4- Listar moviles de acuerdo a una ubicacion");
 			System.out.println("5- Cerrar");
-			//op = "";
+			
 			op = sc.nextLine();
-			//System.out.println("la operacion elegida es: "+op);
 			if (op.equals("1")) {
 				System.out.println("Ingrese el id y tipo del movil");
 				int id= sc.nextInt();
@@ -138,5 +120,6 @@ public class main {
 		}
 
 	}
+	
 
 }
